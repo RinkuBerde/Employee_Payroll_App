6401b92f3fff1a4643bea34defd75c83f7af0152
rinkuberde <rinkuberde@gmail.com>
2023-03-12 21:12:48 +0530

    Ability to Set Event Listener on Salary Range

---
+++ javascript/payroll.js
@@ -0,0 +1,8 @@
+//UC08----->Ability to Set Event Listener on Salary Range
+const salary = document.querySelector("#salary");
+const output = document.querySelector(".salary-output");
+output.textContent = salary.value;
+salary.addEventListener("input", function () 
+{
+  output.textContent = salary.value;
+});
